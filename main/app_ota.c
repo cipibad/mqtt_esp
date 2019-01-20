9aa8e4b4893822255c3a50493fe3ca962f124f13
Cipi Bad <cipibad@gmail.com>
2019-01-20 22:41:52 +0200

    fix ota indentation

--- main/app_ota.c
+++ main/app_ota.c
@@ -50,12 +50,12 @@ extern const int OTA_BIT;
 /*read buffer by byte still delim ,return read bytes counts*/
 static int read_until(char *buffer, char delim, int len)
 {
-//  /*TODO: delim check,buffer check,further: do an buffer length limited*/
-    int i = 0;
-    while (buffer[i] != delim && i < len) {
-        ++i;
-    }
-    return i + 1;
+  //  /*TODO: delim check,buffer check,further: do an buffer length limited*/
+  int i = 0;
+  while (buffer[i] != delim && i < len) {
+    ++i;
+  }
+  return i + 1;
 }
 
 /* resolve a packet from http socket
@@ -64,30 +64,30 @@ static int read_until(char *buffer, char delim, int len)
  * */
 static bool read_past_http_header(char text[], int total_len, esp_ota_handle_t update_handle)
 {
-    /* i means current position */
-    int i = 0, i_read_len = 0;
-    while (text[i] != 0 && i < total_len) {
-        i_read_len = read_until(&text[i], '\n', total_len);
-        // if we resolve \r\n line,we think packet header is finished
-        if (i_read_len == 2) {
-            int i_write_len = total_len - (i + 2);
-            memset(ota_write_data, 0, BUFFSIZE);
-            /*copy first http packet body to write buffer*/
-            memcpy(ota_write_data, &(text[i + 2]), i_write_len);
+  /* i means current position */
+  int i = 0, i_read_len = 0;
+  while (text[i] != 0 && i < total_len) {
+    i_read_len = read_until(&text[i], '\n', total_len);
+    // if we resolve \r\n line,we think packet header is finished
+    if (i_read_len == 2) {
+      int i_write_len = total_len - (i + 2);
+      memset(ota_write_data, 0, BUFFSIZE);
+      /*copy first http packet body to write buffer*/
+      memcpy(ota_write_data, &(text[i + 2]), i_write_len);
 
-            esp_err_t err = esp_ota_write( update_handle, (const void *)ota_write_data, i_write_len);
-            if (err != ESP_OK) {
-                ESP_LOGE(TAG, "Error: esp_ota_write failed! err=0x%x", err);
-                return false;
-            } else {
-                ESP_LOGI(TAG, "esp_ota_write header OK");
-                binary_file_length += i_write_len;
-            }
-            return true;
-        }
-        i += i_read_len;
+      esp_err_t err = esp_ota_write( update_handle, (const void *)ota_write_data, i_write_len);
+      if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Error: esp_ota_write failed! err=0x%x", err);
+        return false;
+      } else {
+        ESP_LOGI(TAG, "esp_ota_write header OK");
+        binary_file_length += i_write_len;
+      }
+      return true;
     }
-    return false;
+    i += i_read_len;
+  }
+  return false;
 }
 
 int hostname_to_ip(char * hostname , char* ip)
@@ -97,95 +97,95 @@ int hostname_to_ip(char * hostname , char* ip)
 	int i;
 		
 	if ( (he = gethostbyname( hostname ) ) == NULL)
-	{
-		// get the host info
-		ESP_LOGI(TAG, "gethostbyname");
-		return 1;
-	}
+    {
+      // get the host info
+      ESP_LOGI(TAG, "gethostbyname");
+      return 1;
+    }
 
 	addr_list = (struct in_addr **) he->h_addr_list;
 	
 	for(i = 0; addr_list[i] != NULL; i++)
-	{
-		//Return the first one;
-		strcpy(ip , inet_ntoa(*addr_list[i]) );
-		return 0;
-	}
+    {
+      //Return the first one;
+      strcpy(ip , inet_ntoa(*addr_list[i]) );
+      return 0;
+    }
 	
 	return 1;
 }
 
 static bool connect_to_http_server()
 {
-    ESP_LOGI(TAG, "Server IP: %s Server Port:%s", EXAMPLE_SERVER_IP, EXAMPLE_SERVER_PORT);
+  ESP_LOGI(TAG, "Server IP: %s Server Port:%s", EXAMPLE_SERVER_IP, EXAMPLE_SERVER_PORT);
 
-    int  http_connect_flag = -1;
-    struct sockaddr_in sock_info;
+  int  http_connect_flag = -1;
+  struct sockaddr_in sock_info;
 
-    socket_id = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_id == -1) {
-        ESP_LOGE(TAG, "Create socket failed!");
-        return false;
-    }
-
-    char ip[16];
-    hostname_to_ip(EXAMPLE_SERVER_IP, ip);
-    // set connect info
-    memset(&sock_info, 0, sizeof(struct sockaddr_in));
-    sock_info.sin_family = AF_INET;
-    sock_info.sin_addr.s_addr = inet_addr(ip);
-    sock_info.sin_port = htons(atoi(EXAMPLE_SERVER_PORT));
-
-    // connect to http server
-    http_connect_flag = connect(socket_id, (struct sockaddr *)&sock_info, sizeof(sock_info));
-    if (http_connect_flag == -1) {
-        ESP_LOGE(TAG, "Connect to server failed! errno=%d", errno);
-        close(socket_id);
-        return false;
-    } else {
-        ESP_LOGI(TAG, "Connected to server");
-        return true;
-    }
+  socket_id = socket(AF_INET, SOCK_STREAM, 0);
+  if (socket_id == -1) {
+    ESP_LOGE(TAG, "Create socket failed!");
     return false;
+  }
+
+  char ip[16];
+  hostname_to_ip(EXAMPLE_SERVER_IP, ip);
+  // set connect info
+  memset(&sock_info, 0, sizeof(struct sockaddr_in));
+  sock_info.sin_family = AF_INET;
+  sock_info.sin_addr.s_addr = inet_addr(ip);
+  sock_info.sin_port = htons(atoi(EXAMPLE_SERVER_PORT));
+
+  // connect to http server
+  http_connect_flag = connect(socket_id, (struct sockaddr *)&sock_info, sizeof(sock_info));
+  if (http_connect_flag == -1) {
+    ESP_LOGE(TAG, "Connect to server failed! errno=%d", errno);
+    close(socket_id);
+    return false;
+  } else {
+    ESP_LOGI(TAG, "Connected to server");
+    return true;
+  }
+  return false;
 }
 
 static void __attribute__((noreturn)) task_fatal_error()
 {
-    ESP_LOGE(TAG, "Exiting task due to fatal error...");
-    close(socket_id);
-    (void)vTaskDelete(NULL);
+  ESP_LOGE(TAG, "Exiting task due to fatal error...");
+  close(socket_id);
+  (void)vTaskDelete(NULL);
 
-    while (1) {
-        ;
-    }
+  while (1) {
+    ;
+  }
 }
 
 void handle_ota_task(void *pvParameters)
 {
 
-    esp_err_t err;
-    /* update handle : set by esp_ota_begin(), must be freed via esp_ota_end() */
-    esp_ota_handle_t update_handle = 0 ;
-    const esp_partition_t *update_partition = NULL;
+  esp_err_t err;
+  /* update handle : set by esp_ota_begin(), must be freed via esp_ota_end() */
+  esp_ota_handle_t update_handle = 0 ;
+  const esp_partition_t *update_partition = NULL;
 
-    ESP_LOGI(TAG, "Starting OTA example...");
+  ESP_LOGI(TAG, "Starting OTA example...");
 
-    const esp_partition_t *configured = esp_ota_get_boot_partition();
-    const esp_partition_t *running = esp_ota_get_running_partition();
+  const esp_partition_t *configured = esp_ota_get_boot_partition();
+  const esp_partition_t *running = esp_ota_get_running_partition();
 
-    if (configured != running) {
-        ESP_LOGW(TAG, "Configured OTA boot partition at offset 0x%08x, but running from offset 0x%08x",
-                 configured->address, running->address);
-        ESP_LOGW(TAG, "(This can happen if either the OTA boot data or preferred boot image become corrupted somehow.)");
-    }
-    ESP_LOGI(TAG, "Running partition type %d subtype %d (offset 0x%08x)",
-             running->type, running->subtype, running->address);
+  if (configured != running) {
+    ESP_LOGW(TAG, "Configured OTA boot partition at offset 0x%08x, but running from offset 0x%08x",
+             configured->address, running->address);
+    ESP_LOGW(TAG, "(This can happen if either the OTA boot data or preferred boot image become corrupted somehow.)");
+  }
+  ESP_LOGI(TAG, "Running partition type %d subtype %d (offset 0x%08x)",
+           running->type, running->subtype, running->address);
 
-    /* Wait for the callback to set the CONNECTED_BIT in the
-       event group.
-    */
+  /* Wait for the callback to set the CONNECTED_BIT in the
+     event group.
+  */
 
-    while(1) {
+  while(1) {
 
     xEventGroupWaitBits(ota_event_group, OTA_BIT,
                         false, true, portMAX_DELAY);
@@ -193,30 +193,30 @@ void handle_ota_task(void *pvParameters)
 
     /*connect to http server*/
     if (connect_to_http_server()) {
-        ESP_LOGI(TAG, "Connected to http server");
+      ESP_LOGI(TAG, "Connected to http server");
     } else {
-        ESP_LOGE(TAG, "Connect to http server failed!");
-        task_fatal_error();
+      ESP_LOGE(TAG, "Connect to http server failed!");
+      task_fatal_error();
     }
 
     /*send GET request to http server*/
     const char *GET_FORMAT =
-        "GET %s HTTP/1.0\r\n"
-        "Host: %s:%s\r\n"
-        "User-Agent: esp-idf/1.0 esp32\r\n\r\n";
+      "GET %s HTTP/1.0\r\n"
+      "Host: %s:%s\r\n"
+      "User-Agent: esp-idf/1.0 esp32\r\n\r\n";
 
     char *http_request = NULL;
     int get_len = asprintf(&http_request, GET_FORMAT, EXAMPLE_FILENAME, EXAMPLE_SERVER_IP, EXAMPLE_SERVER_PORT);
     if (get_len < 0) {
-        ESP_LOGE(TAG, "Failed to allocate memory for GET request buffer");
-        task_fatal_error();
+      ESP_LOGE(TAG, "Failed to allocate memory for GET request buffer");
+      task_fatal_error();
     }
     int res = send(socket_id, http_request, get_len, 0);
     free(http_request);
 
     if (res < 0) {
-        ESP_LOGE(TAG, "Send GET request to server failed");
-        task_fatal_error();
+      ESP_LOGE(TAG, "Send GET request to server failed");
+      task_fatal_error();
     } else {
       ESP_LOGI(TAG, "Send GET request to server succeeded, file: %s", EXAMPLE_FILENAME);
     }
@@ -228,55 +228,55 @@ void handle_ota_task(void *pvParameters)
 
     err = esp_ota_begin(update_partition, OTA_SIZE_UNKNOWN, &update_handle);
     if (err != ESP_OK) {
-        ESP_LOGE(TAG, "esp_ota_begin failed, error=%d", err);
-        task_fatal_error();
+      ESP_LOGE(TAG, "esp_ota_begin failed, error=%d", err);
+      task_fatal_error();
     }
     ESP_LOGI(TAG, "esp_ota_begin succeeded");
 
     bool resp_body_start = false, flag = true;
     /*deal with all receive packet*/
     while (flag) {
-        memset(text, 0, TEXT_BUFFSIZE);
-        memset(ota_write_data, 0, BUFFSIZE);
-        int buff_len = recv(socket_id, text, TEXT_BUFFSIZE, 0);
-        if (buff_len < 0) { /*receive error*/
-            ESP_LOGE(TAG, "Error: receive data error! errno=%d", errno);
-            task_fatal_error();
-        } else if (buff_len > 0 && !resp_body_start) { /*deal with response header*/
-            memcpy(ota_write_data, text, buff_len);
-            resp_body_start = read_past_http_header(text, buff_len, update_handle);
-        } else if (buff_len > 0 && resp_body_start) { /*deal with response body*/
-            memcpy(ota_write_data, text, buff_len);
-            err = esp_ota_write( update_handle, (const void *)ota_write_data, buff_len);
-            if (err != ESP_OK) {
-                ESP_LOGE(TAG, "Error: esp_ota_write failed! err=0x%x", err);
-                task_fatal_error();
-            }
-            binary_file_length += buff_len;
-            ESP_LOGI(TAG, "Have written image length %d", binary_file_length);
-        } else if (buff_len == 0) {  /*packet over*/
-            flag = false;
-            ESP_LOGI(TAG, "Connection closed, all packets received");
-            close(socket_id);
-        } else {
-            ESP_LOGE(TAG, "Unexpected recv result");
+      memset(text, 0, TEXT_BUFFSIZE);
+      memset(ota_write_data, 0, BUFFSIZE);
+      int buff_len = recv(socket_id, text, TEXT_BUFFSIZE, 0);
+      if (buff_len < 0) { /*receive error*/
+        ESP_LOGE(TAG, "Error: receive data error! errno=%d", errno);
+        task_fatal_error();
+      } else if (buff_len > 0 && !resp_body_start) { /*deal with response header*/
+        memcpy(ota_write_data, text, buff_len);
+        resp_body_start = read_past_http_header(text, buff_len, update_handle);
+      } else if (buff_len > 0 && resp_body_start) { /*deal with response body*/
+        memcpy(ota_write_data, text, buff_len);
+        err = esp_ota_write( update_handle, (const void *)ota_write_data, buff_len);
+        if (err != ESP_OK) {
+          ESP_LOGE(TAG, "Error: esp_ota_write failed! err=0x%x", err);
+          task_fatal_error();
         }
+        binary_file_length += buff_len;
+        ESP_LOGI(TAG, "Have written image length %d", binary_file_length);
+      } else if (buff_len == 0) {  /*packet over*/
+        flag = false;
+        ESP_LOGI(TAG, "Connection closed, all packets received");
+        close(socket_id);
+      } else {
+        ESP_LOGE(TAG, "Unexpected recv result");
+      }
     }
 
     ESP_LOGI(TAG, "Total Write binary data length : %d", binary_file_length);
 
     if (esp_ota_end(update_handle) != ESP_OK) {
-        ESP_LOGE(TAG, "esp_ota_end failed!");
-        task_fatal_error();
+      ESP_LOGE(TAG, "esp_ota_end failed!");
+      task_fatal_error();
     }
     err = esp_ota_set_boot_partition(update_partition);
     if (err != ESP_OK) {
-        ESP_LOGE(TAG, "esp_ota_set_boot_partition failed! err=0x%x", err);
-        task_fatal_error();
+      ESP_LOGE(TAG, "esp_ota_set_boot_partition failed! err=0x%x", err);
+      task_fatal_error();
     }
     ESP_LOGI(TAG, "Prepare to restart system!");
     esp_restart();
 
-    }
+  }
 }
 
