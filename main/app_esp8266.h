0c2db57d3820b8985de3e0376f1d0ec06b7f165c
Cipi Bad <cipibad@gmail.com>
2019-03-26 12:24:58 +0200

    use different on/off defined for LED and RELAY

--- main/app_esp8266.h
+++ main/app_esp8266.h
@@ -2,8 +2,11 @@
 #define APP_ESP8266_H
 
 #define ESP8266
-#define ON 0
-#define OFF 1
+#define RELAY_ON 1
+#define RELAY_OFF 0
+
+#define LED_ON 0
+#define LED_OFF 1
 
 #define MQTT_TIMEOUT 30
 #define MQTT_FLAG_TIMEOUT (MQTT_TIMEOUT * 1000 / portTICK_PERIOD_MS)
