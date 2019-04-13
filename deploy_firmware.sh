02d0126ef84b1c545b23f98309924d6273e1900e
Cipi Bad <cipibad@gmail.com>
2019-04-13 21:06:48 +0300

    fix 1MB flash deploy script

--- deploy_firmware.sh
+++ deploy_firmware.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 clientid=`grep CONFIG_MQTT_CLIENT_ID sdkconfig | cut -d '"' -f 2`
-flash_size=`grep CONFIG_ESPTOOLPY_FLASHSIZE sdkconfig | cut -d '"' -f 2`
+flash_size=`grep CONFIG_ESPTOOLPY_FLASHSIZE= sdkconfig | cut -d '"' -f 2`
 
 if [ "$flash_size" = "1MB" ]; then
     scp ./build/mqtt_ssl.ota.bin 192.168.201.2:/var/www/sw.iot.cipex.ro/${clientid}.bin
