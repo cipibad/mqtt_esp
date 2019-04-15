d2474ebbdda3e3346e5435c29643d3e0a5c1877a
Cipi Bad <cipibad@gmail.com>
2019-04-15 13:32:48 +0300

    using port config

--- main/app_mqtt.c
+++ main/app_mqtt.c
@@ -280,7 +280,7 @@ static void mqtt_subscribe(esp_mqtt_client_handle_t client)
 esp_mqtt_client_handle_t mqtt_init()
 {
   const esp_mqtt_client_config_t mqtt_cfg = {
-    .uri = "mqtts://" CONFIG_MQTT_USERNAME ":" CONFIG_MQTT_PASSWORD "@" CONFIG_MQTT_SERVER,
+    .uri = "mqtts://" CONFIG_MQTT_USERNAME ":" CONFIG_MQTT_PASSWORD "@" CONFIG_MQTT_SERVER ":" CONFIG_MQTT_PORT,
     .event_handle = mqtt_event_handler,
     .cert_pem = (const char *)mqtt_iot_cipex_ro_pem_start,
     .client_id = CONFIG_MQTT_CLIENT_ID,
