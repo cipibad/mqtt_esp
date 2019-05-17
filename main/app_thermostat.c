3486b5fbb3dc8346e7ab8cc96c87c968232c8efc
Cipi Bad <cipibad@gmail.com>
2019-05-17 08:43:54 +0300

    heatingState have to many false info, trying two other algo

--- main/app_thermostat.c
+++ main/app_thermostat.c
@@ -16,6 +16,7 @@
 
 bool thermostatEnabled = false;
 bool heatingEnabled = false;
+bool heatingEnabled2 = false;
 int targetTemperature=23*10; //30 degrees
 int targetTemperatureSensibility=5; //0.5 degrees
 
@@ -26,6 +27,7 @@ extern int32_t wtemperature;
 extern int32_t ctemperature;
 int32_t ctemperature_1 = 0;
 int32_t ctemperature_2 = 0;
+int32_t ctemperature_3 = 0;
 extern EventGroupHandle_t mqtt_event_group;
 extern const int MQTT_INIT_FINISHED_BIT;
 extern const int MQTT_PUBLISHED_BIT;
@@ -67,8 +69,8 @@ void publish_thermostat_state(esp_mqtt_client_handle_t client)
       char data[256];
       memset(data,0,256);
 
-      sprintf(data, "{\"thermostatState\":%d, \"heatingState\":%d}",
-              thermostatEnabled, heatingEnabled);
+      sprintf(data, "{\"thermostatState\":%d, \"heatingState\":%d, \"heatingState2\":%d}",
+              thermostatEnabled, heatingEnabled, heatingEnabled2);
       xEventGroupClearBits(mqtt_event_group, MQTT_PUBLISHED_BIT);
       int msg_id = esp_mqtt_client_publish(client, connect_topic, data,strlen(data), 1, 1);
       if (msg_id > 0) {
@@ -114,10 +116,12 @@ void enableThermostat(esp_mqtt_client_handle_t client)
 void update_thermostat(esp_mqtt_client_handle_t client)
 {
 
-  ESP_LOGI(TAG, "heating state is %d", heatingEnabled);
+  ESP_LOGI(TAG, "heatingEnabled state is %d", heatingEnabled);
+  ESP_LOGI(TAG, "heatingEnabled2 state is %d", heatingEnabled2);
   ESP_LOGI(TAG, "ctemperature_n is %d", ctemperature);
   ESP_LOGI(TAG, "ctemperature_n_1 is %d", ctemperature_1);
   ESP_LOGI(TAG, "ctemperature_n_2 is %d", ctemperature_2);
+  ESP_LOGI(TAG, "ctemperature_n_3 is %d", ctemperature_3);
   ESP_LOGI(TAG, "thermostat state is %d", thermostatEnabled);
   ESP_LOGI(TAG, "wtemperature is %d", wtemperature);
   ESP_LOGI(TAG, "targetTemperature is %d", targetTemperature);
@@ -143,15 +147,19 @@ void update_thermostat(esp_mqtt_client_handle_t client)
       enableThermostat(client);
     }
 
-  if (ctemperature_2 && ctemperature_1 && ctemperature) {//three consecutive valid readings
-    if (!heatingEnabled && ctemperature_2 < ctemperature_1 && ctemperature_1 < ctemperature) { //heating is enabled
+  if (ctemperature_2 && ctemperature_1 && ctemperature) {//three consecutive valid readings with 2 difference
+    if (!heatingEnabled
+        && ((ctemperature_2 + 1) < ctemperature_1
+            && ctemperature_1 < (ctemperature - 1))){ //water is heating 1 3 5
       publish_thermostat_state(client);
       heatingEnabled = true;
       ESP_LOGI(TAG, "heating enabled");
       publish_thermostat_state(client);
     }
 
-    if (heatingEnabled && ctemperature_2 >= ctemperature_1 && ctemperature_1 >= ctemperature) { //heating is disabled
+    if (heatingEnabled
+        && ( (ctemperature_2 + 1) >= ctemperature_1
+             || ctemperature_1 >= (ctemperature - 1 ))) { //heating is disabled   1 3 4
       publish_thermostat_state(client);
       heatingEnabled = false;
       ESP_LOGI(TAG, "heating disabled");
@@ -159,6 +167,33 @@ void update_thermostat(esp_mqtt_client_handle_t client)
 
     }
   }
+
+
+  if (ctemperature_3 && ctemperature_2 && ctemperature_1 && ctemperature) {//four consecutive valid readings
+    if (!heatingEnabled2
+        && ( ctemperature_3 < ctemperature_2
+             && ctemperature_2 < ctemperature_1
+             && ctemperature_1 < ctemperature )){ //water is heating 1 2 3 4
+      publish_thermostat_state(client);
+      heatingEnabled2 = true;
+      ESP_LOGI(TAG, "heating enabled");
+      publish_thermostat_state(client);
+    }
+
+    if (heatingEnabled2
+        && ( ctemperature_3 >= ctemperature_2
+             || ctemperature_2 >= ctemperature_1
+             || ctemperature_1 >= ctemperature )) { //heating is disabled   1 3 4
+      publish_thermostat_state(client);
+      heatingEnabled2 = false;
+      ESP_LOGI(TAG, "heating disabled");
+      publish_thermostat_state(client);
+
+    }
+  }
+
+
+  ctemperature_3 = ctemperature_2;
   ctemperature_2 = ctemperature_1;
   ctemperature_1 = ctemperature;
 }
