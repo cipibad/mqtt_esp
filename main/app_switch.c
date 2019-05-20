e891f4b84b1aa03648b592b46c7aecdcf75fe5fd
Cipi Bad <cipibad@gmail.com>
2019-05-20 17:57:02 +0300

    support sonoff smart switch

--- main/app_switch.c
+++ main/app_switch.c
@@ -4,6 +4,7 @@
 
 
 #include "app_switch.h"
+#include "app_smart_config.h"
 
 #include "driver/gpio.h"
 
@@ -16,9 +17,11 @@ extern QueueHandle_t smartconfigQueue;
 
 static void gpio_isr_handler(void *arg)
 {
-  int n=xTaskGetTickCountFromISR();
+  struct SmartConfigMessage scm;
+  scm.ticks = xTaskGetTickCountFromISR();
+  scm.relayId = (int) arg;
   xQueueSendFromISR(smartconfigQueue
-                    ,( void * )&n
+                    ,( void * )&scm
                     ,NULL);
 
 }
@@ -30,6 +33,7 @@ void gpio_switch_init (void *arg)
   io_conf.intr_type = GPIO_INTR_ANYEDGE;
   //bit mask of the pins, use GPIO4/5 here
   io_conf.pin_bit_mask = (1ULL << CONFIG_MQTT_SWITCHES_NB0_GPIO);
+  io_conf.pin_bit_mask |= (1ULL << CONFIG_MQTT_SWITCHES_NB1_GPIO);
   //set as input mode
   io_conf.mode = GPIO_MODE_INPUT;
   gpio_config(&io_conf);
@@ -37,7 +41,8 @@ void gpio_switch_init (void *arg)
   //install gpio isr service
   gpio_install_isr_service(0);
   //hook isr handler for specific gpio pin
-  gpio_isr_handler_add(CONFIG_MQTT_SWITCHES_NB0_GPIO, gpio_isr_handler, (void *) CONFIG_MQTT_SWITCHES_NB0_GPIO);
+  gpio_isr_handler_add(CONFIG_MQTT_SWITCHES_NB0_GPIO, gpio_isr_handler, (void *) 0);
+  gpio_isr_handler_add(CONFIG_MQTT_SWITCHES_NB1_GPIO, gpio_isr_handler, (void *) 1);
   //hook isr handler for specific gpio pin
 }
 
