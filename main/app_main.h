4ad347f180a38b7a250626fc8b939988201501b6
Cipi Bad <cipibad@gmail.com>
2020-10-13 08:58:22 +0300

    fix thermostat usage for esp8266

--- main/app_main.h
+++ main/app_main.h
@@ -2,13 +2,15 @@
 #define APP_MAIN_H
 
 #if CONFIG_ESPTOOLPY_FLASHSIZE_1MB
-#ifdef CONFIG_MQTT_THERMOSTAT
+#if CONFIG_MQTT_THERMOSTATS_NB > 0
+// this is hack for very specific usage where
+// esp8266 is used to control external relays box (like it is used for thermostat)
 #define RELAY_ON 0
 #define RELAY_OFF 1
-#else //CONFIG_MQTT_THERMOSTAT
+#else // CONFIG_MQTT_THERMOSTATS_NB > 0
 #define RELAY_ON 1
 #define RELAY_OFF 0
-#endif //CONFIG_MQTT_THERMOSTAT
+#endif // CONFIG_MQTT_THERMOSTATS_NB > 0
 
 
 #else //CONFIG_ESPTOOLPY_FLASHSIZE_1MB
