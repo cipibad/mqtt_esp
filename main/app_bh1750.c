1d3a4c5c211be9943c8ff69ae0fe4a1958151cc7
Cipi Bad <cipibad@gmail.com>
2023-12-19 10:21:37 +0200

    added config options for bh1750

--- main/app_bh1750.c
+++ main/app_bh1750.c
@@ -11,8 +11,6 @@
 
 #include "app_bh1750.h"
 
-#define I2C_MASTER_SCL_IO           5                /*!< gpio number for I2C master clock */
-#define I2C_MASTER_SDA_IO           4               /*!< gpio number for I2C master data  */
 #define I2C_MASTER_NUM              I2C_NUM_0        /*!< I2C port number for master dev */
 #define I2C_MASTER_TX_BUF_DISABLE   0                /*!< I2C master do not need buffer */
 #define I2C_MASTER_RX_BUF_DISABLE   0                /*!< I2C master do not need buffer */
@@ -46,18 +44,17 @@ esp_err_t i2c_master_BH1750_init()
     int i2c_master_port = I2C_MASTER_NUM;
     i2c_config_t conf;
     conf.mode = I2C_MODE_MASTER;
-    conf.sda_io_num = I2C_MASTER_SDA_IO;
+    conf.sda_io_num = CONFIG_BH1750_SENSOR_SDA_GPIO;
     conf.sda_pullup_en = 1;
-    conf.scl_io_num = I2C_MASTER_SCL_IO;
+    conf.scl_io_num = CONFIG_BH1750_SENSOR_SCL_GPIO;
     conf.scl_pullup_en = 1;
-    conf.clk_stretch_tick = 300; // 300 ticks, Clock stretch is about 210us, you can make changes according to the actual situation.
+    conf.clk_stretch_tick = CONFIG_BH1750_SENSOR_I2C_CLK_STRETCH_TICK; // 300 ticks, Clock stretch is about 210us, you can make changes according to the actual situation.
     #ifdef CONFIG_TARGET_DEVICE_ESP32
     ESP_ERROR_CHECK(i2c_driver_install(i2c_master_port, conf.mode, 0, 0, 0));
     #else //CONFIG_TARGET_DEVICE_ESP32
     ESP_ERROR_CHECK(i2c_driver_install(i2c_master_port, conf.mode));
     #endif //CONFIG_TARGET_DEVICE_ESP32
     ESP_ERROR_CHECK(i2c_param_config(i2c_master_port, &conf));
-    return ESP_OK;
     return ESP_OK;
 }
 
