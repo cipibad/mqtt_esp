f514f88ce36b3965d7d2a484662d1b5a856d71ea
Ciprian Badescu <cipibad@gmail.com>
2024-02-06 23:59:00 +0200

    implement presence cooldown

--- main/app_presence.c
+++ main/app_presence.c
@@ -15,24 +15,33 @@
 #include "app_relay.h"
 extern QueueHandle_t relayQueue;
 
-extern bool motion_detected;
+extern bool local_motion_detected;
 #ifdef CONFIG_BH1750_SENSOR
 extern uint16_t illuminance;
 #endif // CONFIG_BH1750_SENSOR
 
 bool presence_detected = false;
+bool presence_cooldown = false;
 
 EventGroupHandle_t presenceEventGroup;
 const int PRESENCE_NEW_DATA_BIT = BIT0;
 const int PRESENCE_TIMER_BIT = BIT1;
 
 TimerHandle_t presence_detection_timer = NULL;
+TimerHandle_t presence_cooldown_timer = NULL;
 
 static const char *TAG = "PRESENCE";
 
+void presence_cooldown_timer_callback( TimerHandle_t xTimer )
+{
+    ESP_LOGI(TAG, "cooldown expired, should disable cooldown status now");
+    presence_cooldown = false;
+    xEventGroupSetBits (presenceEventGroup, PRESENCE_NEW_DATA_BIT);
+}
+
 void presence_detection_timer_callback( TimerHandle_t xTimer )
 {
-    ESP_LOGI(TAG, "timer expired, should disable presence status now");
+    ESP_LOGI(TAG, "detection timer expired, should disable presence status now");
     xEventGroupSetBits (presenceEventGroup, PRESENCE_TIMER_BIT);
 }
 
@@ -68,12 +77,25 @@ void app_presence_task(void* pvParameters)
         return;
     }
 
+    presence_cooldown_timer = xTimerCreate( "presence_cooldown_timer",           /* Text name. */
+                    pdMS_TO_TICKS(2 * 1000),  /* Period. */
+                    pdFALSE,                /* Autoreload. */
+                    (void *)0,                  /* no ID. */
+                    presence_cooldown_timer_callback );  /* Callback function. */
+    if (presence_cooldown_timer == NULL) {
+        ESP_LOGE(TAG, "No presence_cooldown_timer found");
+        return;
+    }
+
     EventBits_t mBits;
     while(1) {
         mBits = xEventGroupWaitBits(presenceEventGroup, PRESENCE_NEW_DATA_BIT | PRESENCE_TIMER_BIT,
                                     true, false, portMAX_DELAY);
         if (mBits & PRESENCE_NEW_DATA_BIT) {
-            if (motion_detected
+            if (presence_cooldown)
+                continue;
+
+            if (local_motion_detected
 #ifdef CONFIG_BH1750_SENSOR
                 && illuminance/100 < CONFIG_PRESENCE_AUTOMATION_ILLUMINANCE_THRESHOLD
 #endif // CONFIG_BH1750_SENSOR
@@ -102,7 +124,7 @@ void app_presence_task(void* pvParameters)
                         ESP_LOGE(TAG, "Cannot start presence_detection_timer");
                         continue;
                     }
-                ESP_LOGI(TAG, "Started presence_detection_timer");
+                    ESP_LOGI(TAG, "Started presence_detection_timer");
                 } else {
                     ESP_LOGI(TAG, "Timer already active: presence_detection_timer");
                 }
@@ -110,11 +132,24 @@ void app_presence_task(void* pvParameters)
         } else if (mBits & PRESENCE_TIMER_BIT) {
             presence_detected = false;
             publish_presence_data();
+
+            presence_cooldown = true;
+            if( xTimerIsTimerActive( presence_cooldown_timer ) == pdFALSE ) {
+                if( xTimerStart(presence_cooldown_timer, 0 ) != pdPASS )
+                {
+                    ESP_LOGE(TAG, "Cannot start presence_cooldown_timer");
+                    continue;
+                }
+                ESP_LOGI(TAG, "Started presence_cooldown_timer");
+            } else {
+                ESP_LOGI(TAG, "Timer already active: presence_cooldown_timer");
+            }
+
             struct RelayMessage r = {RELAY_CMD_STATUS, 0, RELAY_STATUS_OFF};
             if (xQueueSend( relayQueue,
                             ( void * )&r,
                             RELAY_QUEUE_TIMEOUT) != pdPASS) {
-            ESP_LOGE(TAG, "Cannot send to relayQueue");
+                ESP_LOGE(TAG, "Cannot send to relayQueue");
             }
         }
     }
